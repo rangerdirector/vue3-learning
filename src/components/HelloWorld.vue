13d78cabe991d83bbee860158f71e1cec46e3608
李儒 <lif_director@163.com>
2022-05-03 23:45:03 +0800

    feat(front): 增加windicss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,6 +29,7 @@ const count = ref(0)
   <button type="button" @click="count++">count is: {{ count }}</button>
 <!--  使用element-plus-->
   <el-button>element-plus</el-button>
+  <el-button class="bg-blue-500">element-plus</el-button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import store from '@/store';
 import {loadAllPlugins} from './plugins';
 
 import 'normalize.css'
+import 'virtual:windi.css'
 
 const app = createApp(App);
 loadAllPlugins(app);
